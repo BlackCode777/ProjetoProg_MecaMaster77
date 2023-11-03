7ce19f309d0192a357f39fab5bb953d41825b431
Anderson Martins <BlackCode77.1980@gmail.com>
2023-11-03 00:17:42 -0300

    criação dos projetos sistema gerenciamento e administração de oficina mecânicas

---
+++ backend-mecamaster77/src/main/java/com/blackcode77/backendmecamaster77/BackendMecamaster77Application.java
@@ -0,0 +1,13 @@
+package com.blackcode77.backendmecamaster77;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMecamaster77Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMecamaster77Application.class, args);
+	}
+
+}
---
+++ backend-mecamaster77/src/test/java/com/blackcode77/backendmecamaster77/BackendMecamaster77ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackcode77.backendmecamaster77;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMecamaster77ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
