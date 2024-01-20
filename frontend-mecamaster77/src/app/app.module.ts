78735ee56c307b3bc4068a04a57deabf64761755
Anderson Martins <BlackCode77.1980@gmail.com>
2024-01-20 20:19:44 -0300

    [ SideNav ] - Atualização do projeto - Atualização do angular para v.17 e atualização das dependências

--- frontend-mecamaster77/src/app/app.module.ts
+++ frontend-mecamaster77/src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatSidenavModule } from '@angular/material/sidenav/sidenav-module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+//* MATERIAL IMPORTS
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +15,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // * MATERIAL IMPORTS
+    // MatSidenavModule,
+    // MatToolbarModule,
+    // MatMenuModule,
+    // MatIconModule,
+    // MatDividerModule,
+    // MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend-mecamaster77/src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+
+  /*
+     production: false,
+    apiUrl: 'http://localhost:3000/api',
+  */
+ 
+};
